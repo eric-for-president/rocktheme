065dd57ec146f019074465946385936b07a8c6c5
Eric Rai <voidmaneric@gmail.com>
2025-08-31 06:37:58 +0000

    updated images

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.home, name="index"),
+    path("about/", views.about, name="about"),
+    path("blog/", views.blog, name="blog"),
+    path("blog-details/", views.blog_details, name="blog_details"),
+    path("contact/", views.contact, name="contact"),
+    path("error/", views.error, name="error"),
+    path("notes/", views.notes, name="notes"),
+    path("pricing/", views.pricing, name="pricing"),
+    path("project/", views.project, name="project"),
+    path("project-details/", views.project_details, name="project_details"),
+    path("review/", views.review, name="review"),
+    path("services/", views.services, name="services"),
+    path("service-details/", views.service_details, name="service_details"),
+    path("team/", views.team, name="team"),
 ]
--- website/views.py
+++ website/views.py
@@ -3,4 +3,56 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html", {})
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def blog(request):
+    return render(request, "blog.html")
+
+
+def blog_details(request):
+    return render(request, "blog-details.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def error(request):
+    return render(request, "error.html")
+
+
+def notes(request):
+    return render(request, "notes.html")
+
+
+def pricing(request):
+    return render(request, "pricing.html")
+
+
+def project(request):
+    return render(request, "project.html")
+
+
+def project_details(request):
+    return render(request, "project-details.html")
+
+
+def review(request):
+    return render(request, "review.html")
+
+
+def services(request):
+    return render(request, "services.html")
+
+
+def service_details(request):
+    return render(request, "service-details.html")
+
+
+def team(request):
+    return render(request, "team.html")
